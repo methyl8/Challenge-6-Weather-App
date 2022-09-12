d607f38cf252c3aa45e8606425fcd79c12468d94
dbyrd <methyl8@comcast.net>
2022-09-12 15:12:58 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var apiURL = ""
+var apiKey = "75ce623957426941f4666814094bb92e"
+var weatherURL = "https://api.openweathermap.org/data/2.5/"
+var todaySearch = "weather?q="
+var fiveDaySearch = "forecast?q="
+
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=75ce623957426941f4666814094bb92e
+
+
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
