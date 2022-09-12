a34c07fe1fe4433f7e1ab0163c7f6f8bfbb5c58e
dbyrd <methyl8@comcast.net>
2022-09-12 16:04:46 -0500

    verify api connected, basic html skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,21 @@ var weatherURL = "https://api.openweathermap.org/data/2.5/"
 var todaySearch = "weather?q="
 var fiveDaySearch = "forecast?q="
 
+
 // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=75ce623957426941f4666814094bb92e
 
-
+function runSearch() {
+    var url
+    var searchText = document.querySelector("#searchInput").value;
+    url = weatherURL + todaySearch + searchText + "&APPID=" + apiKey +"&units=imperial"
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+document.querySelector("#searchBtn").addEventListener("click", runSearch)
